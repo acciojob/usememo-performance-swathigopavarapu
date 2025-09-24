446fc119ff9a5b75690f1850f485a6fd2f87acdd
Swathi Gopavarapu <swathigopavarapu1@gmail.com>
2025-09-24 09:41:21 +0530

    initial commit - UseMeno

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const generateTasks = () => {
   for (let i = 1; i <= 50; i++) {
     tasks.push({
       id: i,
-      text: `Task ${i}`,
+      text: `Todo ${i}`,
       completed: i <= 25,
     });
   }
